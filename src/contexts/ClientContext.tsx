9ee9c67abdff8059b0c49394a9764f56be8ae29f
Mars <speed0939@gmail.com>
2023-04-14 13:45:56 +0800

    disable expolore

--- src/contexts/ClientContext.tsx
+++ src/contexts/ClientContext.tsx
@@ -152,6 +152,7 @@ export function ClientContextProvider({ children }: { children: ReactNode | Reac
       const web3Modal = new Web3Modal({
         projectId: DEFAULT_PROJECT_ID,
         walletConnectVersion: 2,
+        enableExplorer: false,
         mobileWallets: [
           {
             id: '1',
